5346413d18e4221248241d700eac8bca8568f2c0
PabloRSGH <89047034+PabloRSGH@users.noreply.github.com>
2022-06-12 19:54:34 -0500

    Registro de nuevo cliente
    Se ha creado la pagina de registro de nuevo cliente, los estilos se agregaron en formularios.css y las funciones de las validaciones en formularios.js (no incluye CRUD)

---
+++ JS/formularios.js
@@ -0,0 +1,31 @@
+function mayus(e) {
+    e.value = e.value.toUpperCase();
+}
+
+function validarRegistro() {
+    if ((!document.querySelector('input[name="genero"]:checked')))
+        alert('Revise que todos los campos han sido completados');
+    //e.preventDefault();    
+}
+
+window.onload = function () {
+
+    let $regreso = document.getElementById('regresar');
+    $regreso.addEventListener('click', confirmaRegreso);
+
+    function confirmaRegreso() {
+        if (!confirm('Está seguro?')) {
+            event.preventDefault();
+        }
+
+
+    }
+
+    let $limpieza = document.getElementById('boton_limpiar');
+    $limpieza.addEventListener('click', confirmaLimpieza);
+    function confirmaLimpieza() {
+        if (!confirm('Está seguro?')) {
+            event.preventDefault();
+        }
+    }
+}
